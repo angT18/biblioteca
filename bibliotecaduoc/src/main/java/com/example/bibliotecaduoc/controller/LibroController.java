9b4b496fb4bb404be46dfbaa9a111e697527f535
Angel Aldea <ang.aldea@duocuc.cl>
2026-03-25 21:00:08 -0300

    Commit de trabajo en clases

---
+++ bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -0,0 +1,42 @@
+package com.example.bibliotecaduoc.controller;
+
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.services.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/libros")
+public class LibroController {
+    @Autowired
+    private LibroService libroService;
+
+
+    @GetMapping
+    public List<Libro> listaLibros() {
+        return libroService.getLibros();
+    }
+
+    @PostMapping
+    public Libro agregaLibro(@RequestBody Libro libro) {
+        return libroService.saveLibro(libro);
+    }
+
+    @GetMapping("{id}")
+    public Libro buscarLibro(@PathVariable int id) {
+        return libroService.getLibroId(id);
+    }
+
+    @PutMapping("{id}")
+    public Libro actualizarLibro(@PathVariable int id, @RequestBody Libro libro){
+        return libroService.updatLibro(libro);
+    }
+
+    
+    @DeleteMapping("{id}")
+    public String eliminaLibro(@PathVariable int id) {
+        return libroService.deleteLibro(id);
+    }
+}
